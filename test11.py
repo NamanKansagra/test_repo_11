0b6a72592266fcbafa3bc0a57feff97e670e2e1f
Naman Kansagra <playerid77@gmail.com>
2022-10-23 00:00:00 +0530

    data

--- test11.py
+++ test11.py
@@ -3,22 +3,23 @@ import random
 import subprocess
 from datetime import datetime, timedelta
 
-start_date = datetime(2023, 3, 1)
-end_date = datetime(2023, 3, 11)
-
-# Create a list of dates within the specified range
-date_list = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
-
-for i, date in enumerate(date_list):
-    # Create file with name git(i).py
+start_date = datetime(2022, 10, 16)
+end_date = datetime(2023, 4, 15)
+for i in range(1, 100):
     filename = f"git{i}.py"
     with open(filename, "w") as f:
         f.write("# This is a sample file")
 
+    # Generate random date within specified range
+    random_date = start_date + \
+        timedelta(days=random.randint(0, (end_date - start_date).days))
+
+    # Run Git commands to add, commit, and push the file to GitHub with random date
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", "data"])
-    subprocess.run(["git", "commit", "--amend", "--date", f"{date:%Y-%m-%d %H:%M:%S}", "--no-edit"])
+    subprocess.run(["git", "commit", "--amend", "--date",
+                   f"{random_date:%Y-%m-%d %H:%M:%S}", "--no-edit"])
     subprocess.run(["git", "push", "origin", "main"])
 
-    # Print message to confirm file creation and push to GitHub with the date
-    print(f"Created files and pushed to GitHub on {date}")
+    # Print message to confirm file creation and push to GitHub with the random date
+    print(f"Created files and pushed to GitHub on {random_date}")
